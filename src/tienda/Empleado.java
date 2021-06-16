902171626e258d7e0cdf7af4a9855976e549cf05
Daniel Gonzalez B <daniel.gb0319@gmail.com>
2021-06-16 04:27:07 -0500

    Creacion de Programa con Clases Base incluyendo respectivos Bosquejos de Estructuras y Funciones

---
+++ src/tienda/Empleado.java
@@ -0,0 +1,16 @@
+package tienda;
+
+public class Empleado extends Persona {
+    public String idEmpleado;
+    public int salario;
+    public int ventas; // cuantos coches ha vendido
+
+// Metodos
+
+/* crear funcion generarID (int y).   ejemplo de id = E001 , E002 (usar concatenacion y variable "y" para 
+generar ) (variable "y" proviene de main) 
+
+funcion imprimir datos del empleado
+
+*/ 
+}
---
+++ src/tienda/Persona.java
@@ -0,0 +1,16 @@
+package tienda;
+
+public class Persona {
+// Atributos
+    public String nombre;
+    public String aPaterno;
+    public String aMaterno;
+    public int edad;
+    private String curp; // curp en lugar de ine
+    private String domicilio;
+    private int telefono;
+
+    /* crear getter y setter para atributos privados
+    
+    */
+}
