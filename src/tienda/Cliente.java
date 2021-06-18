dab78a91ec74c3e9717732422f5876ec7a184839
AlanVG0523 <avallejog1800@alumno.ipn.mx>
2021-06-18 01:46:28 -0500

    Modifique impresion en Cliente, Empleado, Persona y Vehiculo

--- src/tienda/Cliente.java
+++ src/tienda/Cliente.java
@@ -5,14 +5,23 @@ public class Cliente extends Persona {
     public String idCliente;
 // Metodos
 
-public void generarID(int x){
-    idCliente="C00" + x;
+public void generarIDE(int cliente){
+    if(cliente<1000){
+        // Se genera idCliente Automaticamente   
+            if(cliente<10){
+                idCliente="C00"+cliente;
+            } else if (cliente<100){
+                idCliente="C0"+cliente;
+            } else { idCliente="C"+cliente; }
 }
 
 public void showCliente(){
-    
+    System.out.println("\nDatos del Cliente \n");
+    System.out.println("\nNombre Completo del Empleado: " + nombre + " " + aPaterno + " " + aMaterno );
+    System.out.println("\nEdad: " + edad + "ID Cliente: " + idCliente + "Telefono: " + telefono );
+    System.out.println("\nDireccion: " + domicilio + "CURP: " + curp );
 }
-/* crear funcion generarID (int x).   ejemplo de id = C001 , C002 (usar concatenacion y variable "x" para 
+/* crear funcion generarID (int cliente).   ejemplo de id = C001 , C002 (usar concatenacion y variable "x" para 
 generar ) (variable x proviene de main)
 
 funcion imprimir datos del cliente
